ec41be0faca40284e7277d06eaafdc9b0107b42d
Dazannah <beattius@gmail.com>
2025-07-03 22:00:52 +0200

    added inputerror

---
+++ frontend/src/components/InputError.vue
@@ -0,0 +1,8 @@
+<script setup>
+defineProps(["message"]);
+</script>
+<template>
+  <ul v-if="message">
+    <li v-for="msg in message" class="text-sm text-red-600 dark:text-red-400 space-y-1">{{ msg }}</li>
+  </ul>
+</template>
