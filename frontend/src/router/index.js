2e919bc76015b9570f5ef2ef3c8aaa05d533c99e
Dazannah <beattius@gmail.com>
2025-07-03 22:01:58 +0200

    made routes  conditional rendering

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,22 +1,40 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+function isAuthenticated() {
+  return !!localStorage.getItem("token");
+}
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: () => import("../views/HomeView.vue")
+      redirect: "/dashboard"
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue")
+      path: "/login",
+      name: "login",
+      component: () => import("../views/LoginView.vue"),
+      beforeEnter: (to, from, next) => {
+        if (isAuthenticated()) next({ name: "dashboard" });
+        else next();
+      }
+    },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      component: () => import("../views/DashboardView.vue"),
+      meta: { requiresAuth: true }
     }
   ]
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !isAuthenticated()) {
+    next({ name: "login" });
+  } else {
+    next();
+  }
 });
 
 export default router;
