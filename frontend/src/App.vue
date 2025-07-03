d082fc2f63a6c46a4cd61fb63d4f782cefd97c35
Dazannah <beattius@gmail.com>
2025-07-03 21:59:29 +0200

    added conditional rendering for menu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,32 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+<script>
+import { inject } from "vue";
+import { RouterLink, RouterView, useRouter } from "vue-router";
+
+export default {
+  setup() {
+    const router = useRouter();
+    const isAuthenticated = inject("isAuthenticated");
+
+    return { router, isAuthenticated };
+  },
+  methods: {
+    logOut() {
+      localStorage.removeItem("token");
+      this.isAuthenticated = false;
+
+      this.router.replace("/login");
+    }
+  }
+};
 </script>
 
 <template>
-  <header>
+  <header v-if="this.isAuthenticated">
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
+        <button @click="logOut">Logout</button>
       </nav>
     </div>
   </header>
