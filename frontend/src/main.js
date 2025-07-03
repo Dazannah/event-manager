6a8b425522c11b8f63a90fbffd4aec9d55337b66
Dazannah <beattius@gmail.com>
2025-07-03 22:00:41 +0200

    added global properties and global isAuthenticated

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,24 @@
 import "./assets/main.css";
 import axios from "axios";
 
-import { createApp } from "vue";
+import { createApp, ref } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-axios.defaults.baseURL = import.meta.env.BACKEND_URL;
-axios.defaults.headers.common["Authorization"] = localStorage.getItem("authToken");
+import InputError from "./components/InputError.vue";
+
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+axios.defaults.headers.common["Authorization"] = localStorage.getItem("token");
 axios.defaults.headers.common["Content-Type"] = "application/json";
 
 const app = createApp(App);
+
+app.config.globalProperties.axios = axios;
+
+app.component("InputError", InputError);
+
+const isAuthenticated = ref(!!localStorage.getItem("token"));
+app.provide("isAuthenticated", isAuthenticated);
 
 app.use(router);
 
