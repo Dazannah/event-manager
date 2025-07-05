c99674ab30123669a86b465c554193bde031e275
Dazannah <beattius@gmail.com>
2025-07-05 13:36:52 +0200

    added to login link

--- frontend/src/views/ResetPasswordView.vue
+++ frontend/src/views/ResetPasswordView.vue
@@ -79,7 +79,12 @@ export default {
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
       <form class="space-y-6" @submit.prevent="resetPassword">
         <div>
-          <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+          <div class="flex items-center justify-between">
+            <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+            <div class="text-sm">
+              <RouterLink to="/login" class="font-semibold text-indigo-600 hover:text-indigo-500">To login</RouterLink>
+            </div>
+          </div>
           <div class="mt-2">
             <input type="email" v-model="form.email" autocomplete="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
             <InputError v-bind:message="response.validation_errors?.email" />
