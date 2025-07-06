418ad6b855afea8ba74ed8cd2e613e7be985845d
Dazannah <beattius@gmail.com>
2025-07-06 13:14:57 +0200

    changed some color to indigo

--- frontend/src/views/EventsView.vue
+++ frontend/src/views/EventsView.vue
@@ -181,7 +181,7 @@ export default {
   <div v-else>
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
       <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-indigo-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
             <th scope="col" class="px-6 py-3">Title</th>
             <th scope="col" class="px-6 py-3">Occurrence</th>
@@ -192,7 +192,7 @@ export default {
           </tr>
         </thead>
         <tbody>
-          <tr v-for="event in events" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+          <tr v-for="event in events" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-indigo-50 dark:hover:bg-gray-600">
             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ event.title }}</th>
             <td class="px-6 py-4">{{ localTzDate(event.occurrence) }}</td>
             <td class="px-6 py-4">{{ event.description }}</td>
